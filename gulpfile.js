c5d34b90ad91cb3c2f51d9fecf22409d392da5bb
atikhobaev <merido96@gmail.com>
2014-12-26 14:22:47 +0600

    autoprefixer, concat, less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	concatCSS = require('gulp-concat-css'),
+	minifyCSS = require('gulp-minify-css'),
+	rename = require("gulp-rename"),
+	autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-  // place code for your default task here
+//less
+gulp.task('less', function() {
+	gulp.src('./dev-folder/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./app/css/'));
 });
+
+//css
+gulp.task('css', function() {
+	gulp.src('./app/css/*.css')
+    .pipe(concatCSS('style.css'))
+    .pipe(autoprefixer({
+            browsers: ['last 15 versions']
+        }))
+    .pipe(minifyCSS(''))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest('./app/css/'));
+});
+
+//default
+gulp.task('default',['connect', 'html', 'less', 'css', 'watch']);
