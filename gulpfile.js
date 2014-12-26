60104a4ee1faf3da986e283297f77f3b71080436
atikhobaev <merido96@gmail.com>
2014-12-26 17:29:44 +0600

    bugfix

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,50 @@ var gulp = require('gulp'),
 	concatCSS = require('gulp-concat-css'),
 	minifyCSS = require('gulp-minify-css'),
 	rename = require("gulp-rename"),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer'),
+    connect = require('gulp-connect');
+
+//connect
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+
+//html
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
 
 //less
 gulp.task('less', function() {
-	gulp.src('./dev-folder/less/*.less')
+	gulp.src('./app/less/*.less')
     .pipe(less())
-    .pipe(gulp.dest('./app/css/'));
+    .pipe(gulp.dest('./app/css/test/'))
 });
 
 //css
 gulp.task('css', function() {
-	gulp.src('./app/css/*.css')
-    .pipe(concatCSS('style.css'))
+	gulp.src('./app/css/test/*.css')
+    .pipe(concatCSS('style.min.css'))
     .pipe(autoprefixer({
             browsers: ['last 15 versions']
         }))
     .pipe(minifyCSS(''))
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest('./app/css/'));
+    .pipe(gulp.dest('./app/css/'))
+    .pipe(connect.reload());
 });
+
+//watch
+gulp.task('watch', function () {
+    gulp.watch(['./app/*.html'], ['html']);
+    gulp.watch(['./app/less/*.less'], ['less']);
+    gulp.watch(['./app/css/test/*.css'], ['css']);
+});
+
 
 //default
 gulp.task('default',['connect', 'html', 'less', 'css', 'watch']);
